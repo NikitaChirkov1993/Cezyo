0fbd2f5d812171529719f097be40f844f3731a58
Nikita <chirkov31011993@gmail.com>
2025-01-15 12:08:10 +0500

    Add Raleway and Montserrat fonts to the project

--- my-app/vite.config.ts
+++ my-app/vite.config.ts
@@ -1,7 +1,20 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import react from '@vitejs/plugin-react-swc';
+import path from 'path';
+import { defineConfig } from 'vite';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    hmr: true,
+  },
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, 'src'),
+      '@components': path.resolve(__dirname, 'src/components'),
+      '@utils': path.resolve(__dirname, 'src/utils'),
+      '@ui': path.resolve(__dirname, 'src/ui'),
+      '@helpers': path.resolve(__dirname, 'src/helpers'),
+    },
+  },
+});
