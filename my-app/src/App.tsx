108ee1d9a9b218d59711a23f184b15df856bca05
Nikita <chirkov31011993@gmail.com>
2025-01-15 11:07:54 +0500

    qwe

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,7 +1,7 @@
 import { useState } from 'react'
+import './App.css'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -24,6 +24,7 @@ function App() {
         <p>
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
+        <div>ПРивет</div>
       </div>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
